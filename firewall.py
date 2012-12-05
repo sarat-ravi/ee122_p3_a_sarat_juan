cd5e8aa111861ae9185dbd7359ceec6891f718df
Sarat Tallamraju <sarattallamraju@gmail.com>
2012-12-04 18:44:28 -0800

    reindented firewall.py

--- firewall.py
+++ firewall.py
@@ -1,51 +1,51 @@
 from pox.core import core
-from pox.lib.addresses import * 
+from pox.lib.addresses import *
 from pox.lib.packet import *
 
 # Get a logger
 log = core.getLogger("fw")
 
 class Firewall (object):
-  """
-  Firewall class.
-  Extend this to implement some firewall functionality.
-  Don't change the name or anything -- the eecore component
-  expects it to be firewall.Firewall.
-  """
-  
-  def __init__(self):
     """
-    Constructor.
-    Put your initialization code here.
+    Firewall class.
+    Extend this to implement some firewall functionality.
+    Don't change the name or anything -- the eecore component
+    expects it to be firewall.Firewall.
     """
-    log.debug("Firewall initialized. Hello")
 
-  def _handle_ConnectionIn (self, event, flow, packet):
-    """
-    New connection event handler.
-    You can alter what happens with the connection by altering the
-    action property of the event.
-    """
-    
-    connection = "[" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]"
+    def __init__(self):
+        """
+        Constructor.
+        Put your initialization code here.
+        """
+        log.debug("Firewall initialized. Hello")
+
+    def _handle_ConnectionIn (self, event, flow, packet):
+        """
+        New connection event handler.
+        You can alter what happens with the connection by altering the
+        action property of the event.
+        """
+
+        connection = "[" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]"
 
 
-    log.debug("Allowed Connection %s" %(str(connection))  )
-    event.action.forward = True
+        log.debug("Allowed Connection %s" %(str(connection))  )
+        event.action.forward = True
 
-  def _handle_DeferredConnectionIn (self, event, flow, packet):
-    """
-    Deferred connection event handler.
-    If the initial connection handler defers its decision, this
-    handler will be called when the first actual payload data
-    comes across the connection.
-    """
-    pass
-    
-  def _handle_MonitorData (self, event, packet, reverse):
-    """
-    Monitoring event handler.
-    Called when data passes over the connection if monitoring
-    has been enabled by a prior event handler.
-    """
-    pass
+    def _handle_DeferredConnectionIn (self, event, flow, packet):
+        """
+        Deferred connection event handler.
+        If the initial connection handler defers its decision, this
+        handler will be called when the first actual payload data
+        comes across the connection.
+        """
+        pass
+
+    def _handle_MonitorData (self, event, packet, reverse):
+        """
+        Monitoring event handler.
+        Called when data passes over the connection if monitoring
+        has been enabled by a prior event handler.
+        """
+        pass
