feff51a8c7685cfdcb1b61dd9a8bb47fd8e16363
root <root@debian.(none)>
2012-12-07 18:13:19 -0800

    better documentation

--- firewall.py
+++ firewall.py
@@ -17,7 +17,6 @@ class Firewall (object):
     def __init__(self):
         """
         Constructor.
-        Put your initialization code here.
         """
         log.debug("Firewall initialized. Hello")
         self.banned_ports = self.get_banned_port_numbers()
@@ -26,18 +25,31 @@ class Firewall (object):
         self.banned_ips = {}
         self.var_log("Banned Domains", self.banned_domains)
 
-    def get_http_info(self, packet):
-        if False:
-            return "blah"
+    def unpack_ethernet_packet(self, packet):
+        """
+        returns ip_packet, tcp_packet, and http_data
+        in that order
+        """
+        # gets http_data from packet
         ip_packet = packet.payload
         tcp_packet = ip_packet.payload
         http_data = tcp_packet.payload
 
+        return ip_packet, tcp_packet, http_data
+
+    def get_http_info(self, packet):
+        """
+        Parses http_data and returns useful info,
+        like domain name for instance
+        """
+        ip_packet, tcp_packet, http_data = self.unpack_ethernet_packet(packet=packet)
+
+        # get domain name from http header
         domain = ""
         m = re.search("(?<=Host: ).*", http_data)
-        if m:
-            domain = m.group(0).strip()
+        if m: domain = m.group(0).strip()
 
+        # return misc http info
         result_dict = {}
         result_dict["domain_name"] = domain
         return result_dict
@@ -51,25 +63,69 @@ class Firewall (object):
         connection = "[" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]"
         dest_ip = flow.dst
 
+        # ban requests for banned domains
         if str(dest_ip) in self.banned_ips:
             log.debug("Denied IP %s" %(str(dest_ip)))
             event.action.deny = True
             return
 
-        ip_packet = packet.payload
-        tcp_packet = ip_packet.payload
-        http_data = tcp_packet.payload
-        
+        # ban invalid ports
         dest_port = int(flow.dstport)
-
-        if dest_port in self.banned_ports:
+        if self.is_invalid_or_banned_port(port=dest_port):
             log.debug("Denied Connection %s" %(str(connection))  )
             event.action.deny = True
             return
 
+        # default behavior for every normal connection
         log.debug("Allowed Connection %s" %(str(connection))  )
         event.action.forward = True
         event.action.monitor_forward = True
+
+    def _handle_MonitorData (self, event, packet, reverse):
+        """
+        Monitoring event handler.
+        Called when data passes over the connection if monitoring
+        has been enabled by a prior event handler.
+        """
+        if reverse:
+            """
+            Monitor Incoming Packets
+            """
+            pass
+        else:
+            """
+            Monitor Outgoing Packets
+            """
+            # get domain name
+            http_info = self.get_http_info(packet=packet)
+            domain_name = http_info["domain_name"]
+            hostname = "####SFSfEWGOH(*&#@R#@#R#@R$T(*U))"
+
+            # Log every nontrivial domain requested
+            if not domain_name == '':
+                hostname = domain_name.split(".")[-2]
+                self.var_log("Domain Name", hostname)
+
+            # bans this ip as it maps to a banned domain
+            if hostname in self.banned_domains:
+                ip = packet.payload.dstip
+                self.banned_ips[str(ip)] = True
+                log.debug("Denied Connection to %s" %(str(ip)))
+
+    def is_invalid_or_banned_port(self, port):
+        """
+        Intelligently determines if port is banned or not
+        returns True if port is banned
+        """
+        # obvious invalid case
+        if port < 0 or port > 1023:
+            return True
+
+        # if port is banned according to banned-ports.txt
+        if port in self.banned_ports:
+            return True
+
+        return False
 
     def _handle_DeferredConnectionIn (self, event, flow, packet):
         """
@@ -80,34 +136,6 @@ class Firewall (object):
         """
         pass
 
-    def _handle_MonitorData (self, event, packet, reverse):
-        """
-        Monitoring event handler.
-        Called when data passes over the connection if monitoring
-        has been enabled by a prior event handler.
-        """
-        if reverse:
-            pass
-        else:
-            ip_packet = packet.payload
-            tcp_packet = ip_packet.payload
-            http_data = tcp_packet.payload
-
-            http_info = self.get_http_info(packet=packet)
-            domain_name = http_info["domain_name"]
-
-            # some gibberish that would never in a million years match
-            hostname = "####SFSfEWGOH(*&#@R#@#R#@R$T(*U))"
-
-            if not domain_name == '':
-                hostname = domain_name.split(".")[-2]
-                self.var_log("Domain Name", hostname)
-
-            if hostname in self.banned_domains:
-                ip = packet.payload.dstip
-                self.banned_ips[str(ip)] = True
-                log.debug("Denied Connection to %s" %(str(ip)))
-        #sarat
 
     def var_log(self, name, message, caller="ConnectionIn"):
         string = "(%s) \t %s = %s" %(str(caller),
