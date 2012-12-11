965c51f7202807aaa361c73e16654559b24d0a23
Juan de Joya <juandejoya@gmail.com>
2012-12-11 14:27:25 -0800

    updated juan_firwall.py with _handle_MonitorData, _remove_address_port
    the following two are still not documented, please take a look.

--- juan_firewall.py
+++ juan_firewall.py
@@ -65,7 +65,7 @@ class Firewall (object):
                 
                 # [_HANDLE_CONNECTIONIN]::CONDITION C1.1: We will look into whether we have
                 # the given dstip, dstport as a <dstip : dstport> key in timers. If so, look
-                # at whether length of thebvalues are greater than or equal to 0.
+                # at what length of the values are.
                 if self.timers.has_key((str(dstip), int(dstport))):
                     
                     # [_HANDLE_CONNECTIONIN]::CONDITION C1.1.1: If the length of the value in
@@ -76,9 +76,9 @@ class Firewall (object):
                     # [_HANDLE_CONNECTIONIN]::CONDITION C1.1.2: If the length of the value in
                     # the kv-pair is greater than 0, terminate the execution given
                     # <dstip : dstport> and delete the pair.
-                    elif len(self.timers[(str(dstip), int(dstport))]) > 0:
+                    else:
                         self.timers[(str(dstip), int(dstport))][0].cancel()
-                        del self.timers[(str(dstip), int(dstport))] # <- CHECK IF CORRECT
+                        self.timers[(str(dstip), int(dstport))].pop(0) # <- CHECK IF CORRECT
     
                 # Either way, remove the given destination port from allowed-port values from the
                 # attached to the given dstip key in the allowed_ports dictionary. Allow the
@@ -109,7 +109,6 @@ class Firewall (object):
     pass
 
     
-    
   def _handle_MonitorData (self, event, packet, reverse):
     """
     Monitoring event handler.
@@ -117,101 +116,180 @@ class Firewall (object):
     has been enabled by a prior event handler.
         
     If we get the values of 227 or 229, we want to parse the packet to get the port.
+    Once we get a specific port number, add it to the allowed_ports
+    Start a time for 10 seconds that deletes that port number from self.allowed_ports
         
     """
     
     # [_HANDLE_MONITORDATA].INIT: Initialize the following: Data, Source and Destination IP,
     # Source and Destination ports.
     data = packet.payload.payload.payload
-    srcIP = packet.payload.srcip
-    dstIP = packet.payload.dstip
-    srcPort = packet.payload.payload.srcport
-    dstPort = packet.payload.payload.dstport
+    srcip = packet.payload.srcip
+    dstip = packet.payload.dstip
+    srcport = packet.payload.payload.srcport
+    dstport = packet.payload.payload.dstport
     log.debug("data: " + str(data))
-    log.debug("srcIP: " + str(srcIP))
-    log.debug("dstIP: " + str(dstIP))
-    log.debug("srcPort: " + str(srcPort))
-    log.debug("dstPort: " + str(dstPort))
-      
+    log.debug("srcIP: " + str(srcip))
+    log.debug("dstIP: " + str(dstip))
+    log.debug("srcPort: " + str(srcport))
+    log.debug("dstPort: " + str(dstport))
+    MAX_PORT = 255*256+255
+    
+    # [_HANDLE_MONITORDATA]::CONDITION A: If the Source port is equal to 21, and if we
+    # find a separator \n in the given data,
+    if srcport == 21:
+        # [_HANDLE_MONITORDATA]
+        if "\n" in data:
+            if self.buffer.has_key((srcip, dstport)):
+                data = self.buffer[(srcip), dstport)] + data
+            data_split = data.split("\n")
+            tmp = data_split[-1]
+            self.buffer[(srcip, dstport)] = tmp
+            count = 0
+            # [_HANDLE_MONITORDATA]:: LOOP A: 
+            while (count < (len(data_split) - 1)):
+                tmp = data_split[count]
+                if len(tmp) > 8:
+                    # [_HANDLE_MONITORDATA]:: CONDITION A1
+                    if "229" in tmp[:3]:
+                        log.debug("We have gotten the value of 229. We must parse " + data + ".")
+                        values = (re.compile('\d+')).findall(tmp)
+                        port_value = int(values[len(values)-1])
+                        if self.allowed_ports.has_key(srcip):
+                            self.allowed_ports[srcip].append(port_values)
+                        else:
+                            self.allowed_ports[srcip] = []
+                            self.allowed_ports[srcip].append(port_values)
+                        if self.timers.has_key((srcip, port_value)):
+                            self.timers[(srcip, port_value)].append(Timer(10, self.remove_address_port, args = (srcip, port_value)))
+                        else:
+                            self.timers[(srcip, port_value)] = []
+                            self.timers[(srcip, port_value)].append(Timer(10, self.remove_address_port, args = (srcip, port_value)))
+                        log.debug(self.allowed_ports)
+                        log.debug("Operation concerning value 229 done.")
+                    # [_HANDLE_MONITORDATA]:: CONDITION A2
+                    if "227" in tmp[:3]:
+                        log.debug("We have gotten the value of 227. We must parse " + data ".")
+                        port = re.compile('/d+')
+                        port_value = int(port.findall(tmp)[-2]))*256+int(port.findall(tmp)[-1])
+                        if self.allowed_ports.has_key(srcip):
+                            self.allowed_ports[srcip].append(port_value)
+                        else:
+                            self.allowed_ports[srcip] = []
+                            self.allowed_ports[srcip].append(port_value)
+                        if self.timers.has_key((srcip, port_value)):
+                            self.timers[(srcip, port_value)].append(Timer(10, self._remove_address_port, args = (srcip, port_value)))
+                        else:
+                            self.timers[(srcip, port_value)] = []
+                            self.timers[(srcip, port_value)].append(Timer(10, self._remove_address_port, args = (srcip, port_value)))
+                        log.debug(self.allowed_ports)
+                        log.debug("Operation concerning value 227 done.")
+                    i += 1
+        else:
+            if self.buffer.has_key((srcip, dstport)):
+                tmp = self.buffer[(srcip, dstport)]
+                tmp = tmp + data
+                self.buffer[(srcip, dstport)] = tmp
+            else:
+                self.buffer[(srcip, dstport)] = data
+        event.action.forward = True
+
+  def _remove_address_port(self, dstip, port):
+    if self.timers.has_key((dstip, port)):
+        if len(self.timers[(dstip, port)]) == 0:
+            del self.timers[(dstip, port)]
+        else:
+            self.timers[(dstip, port)].pop(0)
+    if self.allowed_ports.has_key(dstip):
+        if port in self.allowed_ports[dstip]:
+            self.allowed_ports[dstip].remove(port)
+    log.debug("remove address port. Please check: " + self.allowed_ports)
+
+
+
+# ORIGINAL CODE, SEE BELOW
+'''
     # REVISING LATER.
     return
-                
-      
-    ''' 
+    '''
+
+'''
     if not reverse:
-      ip = str(dstIP)
-      extPort = str(dstPort) #made this a string
-      otherPort = str(srcPort)
-      out = True
+    ip = str(dstIP)
+    extPort = str(dstPort) #made this a string
+    otherPort = str(srcPort)
+    out = True
     else:
-      ip = str(srcIP)
-      extPort = str(srcPort) #and this
-      otherPort = str(dstPort)
-      out = False
+    ip = str(srcIP)
+    extPort = str(srcPort) #and this
+    otherPort = str(dstPort)
+    out = False
     #log.debug(str(packet.payload.payload))
     if int(extPort) == 21:
-        m = re.search("227 Entered Passive Mode .*", str(data))
-        if m:            
-            newlineraw = m.group(0)
-            newline = newlineraw.replace("227 Entered Passive Mode (", "")
-            newline = newline.replace(")", "")
-            #log.debug(newline)
-            newportraw = newline.rsplit(',')
-            newport = self.getPortNumber(int(newportraw[4]), int(newportraw[5]))
-            
-            assigned_ip = newportraw[0] + "." + newportraw[1] + "." + newportraw[2] + "." + newportraw[3]
-            
-            self.allowed_ports[newport] = [Timer(10, self.connectionDone, args = (newport)), assigned_ip]  
-            self.data_sizes[newport] = -1
-            #log.debug("NEW ASSIGNED IP: ")  
-            #log.debug(assigned_ip)        
-        l = re.search("229 Entering extended passive mode.*", str(data))
-        
-        if l:            
-            newlineraw = l.group(0)
-            newline = newlineraw.replace("229 Entering extended passive mode (|||", "")
-            newline = newline.replace("|).", "")
-            
-            newport = int(newline)
-            
-            self.allowed_ports[newport] = [Timer(10, self.connectionDone, args = (newport))] 
-            self.data_sizes[newport] = -1
+    m = re.search("227 Entered Passive Mode .*", str(data))
+    if m:
+    newlineraw = m.group(0)
+    newline = newlineraw.replace("227 Entered Passive Mode (", "")
+    newline = newline.replace(")", "")
+    #log.debug(newline)
+    newportraw = newline.rsplit(',')
+    newport = self.getPortNumber(int(newportraw[4]), int(newportraw[5]))
+    
+    assigned_ip = newportraw[0] + "." + newportraw[1] + "." + newportraw[2] + "." + newportraw[3]
+    
+    self.allowed_ports[newport] = [Timer(10, self.connectionDone, args = (newport)), assigned_ip]
+    self.data_sizes[newport] = -1
+    #log.debug("NEW ASSIGNED IP: ")
+    #log.debug(assigned_ip)
+    l = re.search("229 Entering extended passive mode.*", str(data))
+    
+    if l:
+    newlineraw = l.group(0)
+    newline = newlineraw.replace("229 Entering extended passive mode (|||", "")
+    newline = newline.replace("|).", "")
+    
+    newport = int(newline)
+    
+    self.allowed_ports[newport] = [Timer(10, self.connectionDone, args = (newport))]
+    self.data_sizes[newport] = -1
     else:
-        #parse TCP here
-        #fromPort, toPort, seqNum
-        junk = str(packet.payload.payload)
-        junk = junk.replace("{","")
-        junks = junk.rsplit(">")
-        fromPort = junks[0]
-        junk = junks[1]
-        junks = junk.rsplit("} seq:")
-        toPort = junks[0]
-        junk = junks[1]
-        junks = junk.rsplit(" ack:")
-        seqNum = junks[0]
-        junk = junks[1]
-        junks = junk.rsplit(" f:")
-        ackNum = junks[0]
-        if int(fromPort) == int(extPort):
-            if self.data_sizes[int(extPort)] == -1:
-                self.data_sizes[int(extPort)] = int(seqNum)
-            elif self.data_sizes[int(extPort)] + 1000000 < int(seqNum):
-                tcppacket = packet.payload.payload
-                tcppacket.srcport = dstPort
-                tcppacket.dstport = srcPort
-                tcppacket.seq = int(ackNum)
-                tcppacket.ack = int(seqNum) + 1460
-                tcppacket.RST=True
-                event.send(tcppacket, reverse=False)
+    #parse TCP here
+    #fromPort, toPort, seqNum
+    junk = str(packet.payload.payload)
+    junk = junk.replace("{","")
+    junks = junk.rsplit(">")
+    fromPort = junks[0]
+    junk = junks[1]
+    junks = junk.rsplit("} seq:")
+    toPort = junks[0]
+    junk = junks[1]
+    junks = junk.rsplit(" ack:")
+    seqNum = junks[0]
+    junk = junks[1]
+    junks = junk.rsplit(" f:")
+    ackNum = junks[0]
+    if int(fromPort) == int(extPort):
+    if self.data_sizes[int(extPort)] == -1:
+    self.data_sizes[int(extPort)] = int(seqNum)
+    elif self.data_sizes[int(extPort)] + 1000000 < int(seqNum):
+    tcppacket = packet.payload.payload
+    tcppacket.srcport = dstPort
+    tcppacket.dstport = srcPort
+    tcppacket.seq = int(ackNum)
+    tcppacket.ack = int(seqNum) + 1460
+    tcppacket.RST=True
+    event.send(tcppacket, reverse=False)
     '''
-                
-                
+
+
+'''
   def connectionDone(self, port):
       del self.allowed_ports[port]
-            
+    
   def getPortNumber(self, num1, num2):
       hex1 = hex(num1)
       hex2_raw = hex(num2)
       hex2 = hex2_raw[2:]
       hex_total = hex1 + hex2
       return int(hex_total, 16)
+'''
